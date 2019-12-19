02be141f652d10247eed38c00f8fd21a79cdd371
emblab <emblab@westlake.edu.cn>
2019-12-19 09:40:14 +0800

    python days100

---
+++ 100Days/Day01.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Created : 2019/12/19 9:21
+# Author  : zhangguoqing
+# E-mail  : zhangguoqing84@westlake.edu.cn
+
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('red')
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
